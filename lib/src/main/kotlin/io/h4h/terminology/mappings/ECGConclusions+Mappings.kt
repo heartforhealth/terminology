37cb36bfd837595292f4aa5b983903b139fd64da
ssaveskih4h <s.saveski@heartforhealth.com>
2024-06-27 12:26:36 +0200

    added some mappings for ECGConclusions

--- lib/src/main/kotlin/io/h4h/terminology/mappings/ECGConclusions+Mappings.kt
+++ lib/src/main/kotlin/io/h4h/terminology/mappings/ECGConclusions+Mappings.kt
@@ -7,30 +7,28 @@ import io.h4h.terminology.codesets.KardiaEKGClassification
 import io.h4h.terminology.codesets.KardiaEKGClassification.*
 
 
-
-
 fun AppleWatchEKGClassification.toECGConclusion(): List<ECGConclusions> = when (this) {
-    notSet -> TODO()
+    notSet -> listOf(ECGConclusions.Unclassified)
     sinusRhythm -> listOf(ECGConclusions.SinusRhythm)
     atrialFibrillation -> listOf(ECGConclusions.AtrialFibrillation)
-    inconclusiveLowHeartRate -> TODO()
-    inconclusiveHighHeartRate -> TODO()
-    inconclusivePoorReading -> TODO()
-    inconclusiveOther -> TODO()
-    unrecognized -> TODO()
+    inconclusiveLowHeartRate -> listOf(ECGConclusions.SinusBradycardia)
+    inconclusiveHighHeartRate -> listOf(ECGConclusions.SinusTachycardia)
+    inconclusivePoorReading -> listOf(ECGConclusions.Noise)
+    inconclusiveOther -> listOf(ECGConclusions.Unclassified)
+    unrecognized -> listOf(ECGConclusions.Noise)
 }
 
 
 fun KardiaEKGClassification.toECGConclusion(): List<ECGConclusions> = when (this) {
-    normal -> TODO()
+    normal -> listOf(ECGConclusions.SinusRhythm)
     sinus_rhythm -> listOf(ECGConclusions.SinusRhythm)
     afib -> listOf(ECGConclusions.AtrialFibrillation)
-    bradycardia -> TODO()
-    no_analysis -> TODO()
+    bradycardia -> listOf(ECGConclusions.SinusTachycardia)
+    no_analysis -> listOf(ECGConclusions.Unclassified)
     sinus_rhythm_multiple_pacs -> listOf(ECGConclusions.SinusRhythm, ECGConclusions.PrematureAtrialComplexes)
     sinus_rhythm_multiple_pvcs -> listOf(ECGConclusions.SinusRhythm, ECGConclusions.PrematureVentricularComplexes)
-    sinus_rhythm_wide_qrs -> TODO()
-    tachycardia -> TODO()
+    sinus_rhythm_wide_qrs -> listOf(ECGConclusions.SinusRhythm, ECGConclusions.BundleBranchBlock)
+    tachycardia -> listOf(ECGConclusions.SinusTachycardia)
     too_long -> TODO()
     too_short -> TODO()
     unclassified -> TODO()
